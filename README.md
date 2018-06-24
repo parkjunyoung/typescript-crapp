8361c2d7527ffd8a60ed089be2bb78cb29987e1b
parkjunyoung <pjoon86@naver.com>
2018-06-24 23:30:14 +0900

    css적용

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ yarn add redux react-redux immutable redux-actions
 ```sh
 yarn add --dev @types/react-redux @types/redux-actions
 ```
+
+```sh
+yarn add --dev typings-for-css-modules-loader
+```
--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import * as styles from './Posts.css';
 
 const Posts: React.SFC = () => (
-    <div>
+    <div className={styles.box}>
         Posts 입니다.
     </div>
 );
